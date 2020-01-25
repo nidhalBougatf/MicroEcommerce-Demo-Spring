1d9bd90ba256abff4d4dc2190576152f380f688c
nidhal.bougatf@esprit.tn <nidhal.bougatf@esprit.tn>
2020-01-25 02:29:03 +0100

    Added static/dynamic filtering exmaple

--- src/main/java/com/ecommerce/microecommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microecommerce/dao/ProductDao.java
@@ -5,8 +5,7 @@ import com.ecommerce.model.Product;
 import java.util.List;
 
 public interface ProductDao {
-    // Names of methods aren't randomly chosen
-    // For more details : https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+    // Names of methods aren't randomly chosen, for more details : https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
     public List<Product> findAll();
     public Product findById(int id);
     public Product save(Product product);
--- src/main/java/com/ecommerce/model/Product.java
+++ src/main/java/com/ecommerce/model/Product.java
@@ -1,22 +1,32 @@
 package com.ecommerce.model;
 
-// This Bean (or JavaBean)
-// Constructor , Getters & Setters are mandatory
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+// Note:  In this Bean (or JavaBean), Constructor, Getters & Setters are mandatory
+
+
+//@JsonIgnoreProperties(value={"id","providerPrice"}) // static filter method 2: In case we have multiple properties to hide
+//@JsonFilter("MyDynamicFilter") // 1/ a dynmaic filter used in case we want to hide a specific property, depending on something (type of users for example) , 2/ Next step , add the filter & edit 'listProducts()' method in ProductController
 public class Product {
 
 
     private int id;
     private String name;
     private int price;
+    //@JsonIgnore // static filter method 1 , used to hide this property while displaying ( replaced later by the dynamic filter )
+    private int providerPrice;
 
 
     public Product() {
     }
 
-    public Product(int id, String name, int price) {
+    public Product(int id, String name, int price, int providerPrice) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.providerPrice=providerPrice;
     }
 
 
@@ -26,6 +36,7 @@ public class Product {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", price=" + price +
+                ", providerPrice=" + providerPrice +
                 '}';
     }
 
@@ -53,7 +64,7 @@ public class Product {
         this.price = price;
     }
 
+    public int getProviderPrice() {  return providerPrice; }
 
-
-
+    public void setProviderPrice(int providerPrice) { this.providerPrice = providerPrice;  }
 }
